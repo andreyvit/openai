587b6daf7ec0df60476df32985e95357bd0c766f
Andrey Tarantsov <andrey@tarantsov.com>
2024-04-11 10:57:58 +0400

    Support for modern models

--- common.go
+++ common.go
@@ -2,11 +2,18 @@ package openai
 
 import (
 	"fmt"
+	"regexp"
 	"strings"
 )
 
 const (
-	// ModelChatGPT4 is the current best chat model, gpt-4, with 8k context (aka prompt+completion).
+	// ModelChatGPT4Turbo is the current best chat model, gpt-4-turbo, with 128k context.
+	ModelChatGPT4Turbo = "gpt-4-turbo"
+
+	// ModelChatGPT4TurboPreview is the preview of GPT-4 Turbo, with 128k context.
+	ModelChatGPT4TurboPreview = "gpt-4-turbo-preview"
+
+	// ModelChatGPT4 is the current best chat model, gpt-4, with 8k context.
 	ModelChatGPT4 = "gpt-4"
 
 	// ModelChatGPT4With32k is a version of ModelChatGPT4 with a 32k context.
@@ -18,7 +25,7 @@ const (
 	// ModelDefaultChat is a chat model used by default. This is going to be set to whatever
 	// default choice the author of this library feels appropriate going forward, but really,
 	// you should be specifying a specific model like ModelChatGPT4 or ModelChatGPT35Turbo.
-	ModelDefaultChat = ModelChatGPT35Turbo
+	ModelDefaultChat = ModelChatGPT4Turbo
 
 	// ModelDefaultCompletion is the current best instruction-following model for text completion.
 	// Not recommended for basically anything any more because gpt-3.5-turbo is 10x cheaper and just as good.
@@ -31,12 +38,25 @@ const (
 	// ModelBaseDavinci is an older GPT 3 (not GPT 3.5) family base model. Only useful for fine-tuning.
 	ModelBaseDavinci = "davinci"
 
-	// ModelEmbeddingAda002 is the best embedding model so far.
+	// ModelEmbedding3Large is the best embedding model so far.
+	ModelEmbedding3Large = "text-embedding-3-large"
+
+	// ModelEmbedding3Small is a better version of ModelEmbeddingAda002.
+	ModelEmbedding3Small = "text-embedding-3-small"
+
+	// ModelEmbeddingAda002 is the original embedding model, its use is no longer recommended.
 	ModelEmbeddingAda002 = "text-embedding-ada-002"
 
 	modelChatGPT35TurboPrefix  = "gpt-3.5-turbo-"
 	modelChatGPT4Prefix        = "gpt-4-"
 	modelChatGPT4With32kPrefix = "gpt-4-32k-"
+)
+
+var (
+	gpt4TurboSnapshotRe   = regexp.MustCompile(`^gpt-4-turbo-\d{4}-\d{2}-\d{2}$`)
+	gpt35TurboSnapshotRe  = regexp.MustCompile(`^gpt-3.5-turbo-\d{4}$`)
+	gpt4With32kSnapshotRe = regexp.MustCompile(`^gpt-4-32k-\d{4}$`)
+	gpt4SnapshotRe        = regexp.MustCompile(`^gpt-4-\d{4}$`)
 )
 
 // MaxTokens returns the maximum number of tokens the given model supports. This is a sum of
@@ -49,13 +69,15 @@ func MaxTokens(model string) int {
 		return 4000 // from docs: https://platform.openai.com/docs/models/gpt-3-5
 	case "text-davinci-003":
 		return 4097
-	case ModelChatGPT35Turbo, "gpt-3.5-turbo-0301":
+	case ModelChatGPT35Turbo:
 		return 4096
 	case ModelChatGPT4:
 		return 8192
 	case ModelChatGPT4With32k:
 		return 32768
-	case "text-embedding-ada-002":
+	case ModelChatGPT4Turbo, ModelChatGPT4TurboPreview, "gpt-4-1106-preview", "gpt-4-0125-preview":
+		return 128000
+	case ModelEmbeddingAda002, ModelEmbedding3Small, ModelEmbedding3Large:
 		return 8192
 	default:
 		if base, _, ok := strings.Cut(model, ":ft-"); ok {
@@ -68,7 +90,7 @@ func MaxTokens(model string) int {
 	}
 }
 
-// Price is an amount in 1/1_000_000 of a cent. I.e. $0.002 per 1K tokens = Price(200) per token.
+// Price is an amount in 1/1_000_000 of a cent. I.e. $2 per 1M tokens = $0.002 per 1K tokens = Price(200) per token.
 type Price int64
 
 // String formats the price as dollars and cents, e.g. $3.14.
@@ -80,12 +102,14 @@ func (p Price) String() string {
 // tokens with the given model.
 func Cost(promptTokens, completionTokens int, model string) Price {
 	switch model {
+	case ModelChatGPT4TurboPreview, "gpt-4-1106-preview", "gpt-4-0125-preview":
+		return Price(promptTokens)*1000 + Price(completionTokens)*3000
 	case ModelChatGPT4:
 		return Price(promptTokens)*3000 + Price(completionTokens)*6000
 	case ModelChatGPT4With32k:
 		return Price(promptTokens)*6000 + Price(completionTokens)*12000
 	case ModelChatGPT35Turbo:
-		return Price(promptTokens+completionTokens) * 200
+		return Price(promptTokens)*50 + Price(completionTokens)*150
 	case "davinci", "text-davinci-003":
 		return Price(promptTokens+completionTokens) * 2000
 	case "curie", "text-curie-001":
@@ -94,8 +118,12 @@ func Cost(promptTokens, completionTokens int, model string) Price {
 		return Price(promptTokens+completionTokens) * 50
 	case "ada", "text-ada-001":
 		return Price(promptTokens+completionTokens) * 40
-	case "text-embedding-ada-002":
-		return Price(promptTokens+completionTokens) * 40
+	case ModelEmbeddingAda002:
+		return Price(promptTokens+completionTokens) * 10
+	case ModelEmbedding3Small:
+		return Price(promptTokens+completionTokens) * 2
+	case ModelEmbedding3Large:
+		return Price(promptTokens+completionTokens) * 13
 	case "code-davinci-002", "text-davinci-002":
 		return Price(promptTokens+completionTokens) * 2000 // just a guess; https://openai.com/pricing doesn't say anything
 	default:
@@ -140,22 +168,19 @@ func FineTuningCost(tokens int, model string) Price {
 }
 
 func snapshotToGeneric(model string) string {
-	if hasSnapshotPrefix(model, ModelChatGPT4) {
-		return ModelChatGPT4
+	if gpt4TurboSnapshotRe.MatchString(model) {
+		return ModelChatGPT4Turbo
 	}
-	if hasSnapshotPrefix(model, ModelChatGPT4With32k) {
-		return ModelChatGPT4With32k
-	}
-	if hasSnapshotPrefix(model, ModelChatGPT35Turbo) {
+	if gpt35TurboSnapshotRe.MatchString(model) {
 		return ModelChatGPT35Turbo
 	}
+	if gpt4With32kSnapshotRe.MatchString(model) {
+		return ModelChatGPT4With32k
+	}
+	if gpt4SnapshotRe.MatchString(model) {
+		return ModelChatGPT4
+	}
 	return ""
-}
-
-// hasSnapshotPrefix checks if the model name starts with (prefix + "-").
-func hasSnapshotPrefix(model, prefix string) bool {
-	p := len(prefix)
-	return len(model) >= p+1 && model[0:p] == prefix && model[p] == '-'
 }
 
 // Credentials are used to authenticate with OpenAI.
